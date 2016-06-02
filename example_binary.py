d060d8bb42b3c646019dbbd01b04b3b58c97bb3d
bloemist <bloemistwashere@gmail.com>
2016-06-02 17:10:32 +0000

    some renaming

---
+++ example_binary.py
@@ -0,0 +1,11 @@
+import lumos
+
+
+
+for i in xrange(1024):
+	bina = format(i, 'b')[::-1]
+	#print(bina)
+	bina = bina.replace('0','000000')
+	bina = bina.replace('1','FFFFFF')
+	#print(bina)
+	lumos.push(bina)
